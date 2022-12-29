a866cdb49b6ea3a1a9c619c2ff0fc7aa13240083
Dave Bray <david@brayworth.com.au>
2022-12-29 10:41:58 +1000

    add info method

---
+++ src/app/home/controller.php
@@ -0,0 +1,21 @@
+<?php
+/*
+ * David Bray
+ * BrayWorth Pty Ltd
+ * e. david@brayworth.com.au
+ *
+ * MIT License
+ *
+*/
+
+namespace home;
+
+use bravedave\esse\controller\home;
+
+class controller extends home {
+
+  public function info() {
+
+    phpinfo();
+  }
+}
